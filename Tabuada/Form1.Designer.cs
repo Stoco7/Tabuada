e14e36086e2a4227851c624ee0e776cd32b473bd
Eduardo Stoco <stoco.eduardo@gmail.com>
2025-02-25 14:04:36 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Tabuada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnGerarTabuada = new System.Windows.Forms.Button();
+            this.lstTabuada = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um Número";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(36, 74);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(159, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnGerarTabuada
+            // 
+            this.btnGerarTabuada.Location = new System.Drawing.Point(120, 110);
+            this.btnGerarTabuada.Name = "btnGerarTabuada";
+            this.btnGerarTabuada.Size = new System.Drawing.Size(75, 23);
+            this.btnGerarTabuada.TabIndex = 2;
+            this.btnGerarTabuada.Text = "Tabuada";
+            this.btnGerarTabuada.UseVisualStyleBackColor = true;
+            this.btnGerarTabuada.Click += new System.EventHandler(this.btnGerarTabuada_Click);
+            // 
+            // lstTabuada
+            // 
+            this.lstTabuada.FormattingEnabled = true;
+            this.lstTabuada.Location = new System.Drawing.Point(37, 151);
+            this.lstTabuada.Name = "lstTabuada";
+            this.lstTabuada.Size = new System.Drawing.Size(159, 134);
+            this.lstTabuada.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 318);
+            this.Controls.Add(this.lstTabuada);
+            this.Controls.Add(this.btnGerarTabuada);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnGerarTabuada;
+        private System.Windows.Forms.ListBox lstTabuada;
+    }
+}
+
---
+++ Tabuada/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabuada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerarTabuada_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtém o número digitado
+                int numero = Convert.ToInt32(txtNumero.Text);
+
+                // Limpa a lista antes de exibir a nova tabuada
+                lstTabuada.Items.Clear();
+
+                // Gera a tabuada de 1 a 10
+                for (int i = 1; i <= 10; i++)
+                {
+                    lstTabuada.Items.Add($"{numero} x {i} = {numero * i}");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira um número inteiro válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro inesperado: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
